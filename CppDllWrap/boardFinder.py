64fd5ceb4d9079d65641177b112174cf61a986ca
Darko <darkosarajkic997@gmail.com>
2021-10-01 17:00:57 +0200

    Python wrapper for BoardFinderLibrary DLL and test script for wrapper

--- CppDllWrap/boardFinder.py
+++ CppDllWrap/boardFinder.py
@@ -2,12 +2,166 @@ import ctypes
 import numpy as np
 import configparser
 
+MAX_ANGLE = 180
+MAX_LINES = 500
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-board_finder_lib=ctypes.CDLL(config['Resources']['dll'])
+board_finder_lib = ctypes.CDLL(config['Resources']['dll'])
 
 
-def filter_lines(lines,number_of_lines):
-    filter_function=board_finder_lib.filterLines
-    filter_function.argtypes=[np.ctypeslib.ndpointer(image_data_type),ctypes.c_int32,ctypes.c_int32]
+def add_value_to_rolling_max(value, rolling_max, range, max_angle=MAX_ANGLE):
+    """Increment value of rolling max for values near angle
+
+    Args:
+        value (integer):  Angle
+        rolling_max (numpy array, dtype=np.int32): Array of integers that represent rolling max
+        range (integer): Range for which to increment rolling max
+        max_angle (integer, optional): Maximum index of rollingMax. Defaults to MAX_ANGLE.
+    """
+    function = board_finder_lib.addValueToRollingMax
+    function.argtypes = [ctypes.c_int32, np.ctypeslib.ndpointer(
+        ctypes.c_int32), ctypes.c_int32, ctypes.c_int32]
+    function(value, rolling_max, range, max_angle)
+
+
+def find_max(values, min_val=0, max_val=MAX_ANGLE):
+    """Finds max value in array on index range from minVal to maxVal
+
+    Args:
+        values (numpy array, dtype=np.int32): Array in which to look for maximum
+        min_val (integer, optional):  Minimal index. Defaults to 0.
+        max_val (integer, optional): Maximal index. Defaults to MAX_ANGLE.
+
+    Returns:
+        (integer): Maximum value
+    """
+    function = board_finder_lib.findMax
+    function.argtypes = [np.ctypeslib.ndpointer(
+        ctypes.c_int32), ctypes.c_int32, ctypes.c_int32]
+    function.restype = ctypes.c_int32
+    return function(values, min_val, max_val)
+
+
+def select_lines_near_peak(lines, number_of_lines, peak, range, selected_lines, max_lines_selected=MAX_LINES):
+    """Select all lines from array near peak angle
+
+    Args:
+        lines (numpy array, dtype=np.int32): Array of lines, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+        number_of_lines (integer): Number of lines in array
+        peak (integer):  Angle that represents peak near which lines are selected
+        range (integer): Width of range from which to select lines [peak-range, peak+range]
+        selected_lines (numpy array, dtype=np.int32): Lines that are selected, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+        max_lines_selected (integer, optional): Maximum number of lines that could be selected. Defaults to MAX_LINES.
+
+    Returns:
+        (integer): Number of selected lines
+    """
+    function = board_finder_lib.selectLinesNearPeak
+    function.argtypes = [np.ctypeslib.ndpointer(
+        ctypes.c_int32), ctypes.c_int32, ctypes.c_int32, np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32]
+    function.restype = ctypes.c_int32
+    return function(lines, number_of_lines, peak, range, selected_lines, max_lines_selected)
+
+
+def find_two_biggest_clusters_of_lines(lines, number_of_lines, rolling_range, peak_width, max_lines_selected, horizontal_lines, vertical_lines):
+    """Find two group of lines that represent horizontal and vertical lines
+
+    Args:
+        lines (numpy array, dtype=np.int32): Array of lines, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+        number_of_lines (integer): Number of lines in array
+        rolling_range (integer): Range in witch to increment max
+        peak_width (integer): Range of angles from witch to select lines near peak angle
+        max_lines_selected (integers): Maximum possible number of lines to be selected
+        horizontal_lines (numpy array, dtype=np.int32): Array of lines, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+        vertical_lines (numpy array, dtype=np.int32): Array of lines, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+
+    Returns:
+        (integer),(integer): Number of "horizontal" lines, NUmber of "vertical" lines
+    """
+    function = board_finder_lib.findTwoBiggestClustersOfLines
+    number_of_horizontal_lines=ctypes.c_int(0)
+    number_of_vertical_lines=ctypes.c_int(0)
+    function.argtypes = [np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32, ctypes.c_int32,ctypes.c_int32,ctypes.c_int32, np.ctypeslib.ndpointer(ctypes.c_int32),np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int)]
+    function(lines, number_of_lines, rolling_range, peak_width, max_lines_selected, horizontal_lines, vertical_lines,ctypes.byref(number_of_horizontal_lines),ctypes.byref(number_of_vertical_lines))
+    return number_of_horizontal_lines.value, number_of_vertical_lines.value
+
+def remove_cluster_mark(clusters, size, mark):
+    """Removes specific mark of cluster from array that contains cluster marks
+
+    Args:
+        clusters (numpy array, dtype=np.int32): Array of cluster marks
+        size (integer): Size of cluster mark array
+        mark (integer): Mark that need to be removed
+    """
+    function=board_finder_lib.removeClusterMark
+    function.argtypes=[np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32, ctypes.c_int32]
+    function(clusters,size,mark)
+
+def lines_DBSCAN(lines, number_of_lines, clusters, min_clusters=2, theta_delta=6,rho_delta=10):
+    """DBSCAN algorithm for lines, finds similar lines and remove outliers
+
+    Args:
+        lines (numpy array, dtype=np.int32): Array of lines, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+        number_of_lines (integer):  Number of lines in array
+        clusters (numpy array, dtype=np.int32):  Array of clusters marks for each lines, outliers have cluster mark -1
+        min_clusters (integer, optional): Minimum number of similar lines that forms cluster. Defaults to 2.
+        theta_delta (integer, optional): Maximum difference in theta that could be between two similar lines. Defaults to 6.
+        rho_delta (integer, optional): Maximum difference in rho that could be between two similar lines. Defaults to 10.
+
+    Returns:
+        (integer): Number of clusters
+    """
+    function=board_finder_lib.linesDBSCAN
+    function.argtypes=[np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32,np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32, ctypes.c_int32, ctypes.c_int32]
+    function.restype = ctypes.c_int32
+    return function(lines,number_of_lines,clusters,min_clusters,theta_delta,rho_delta)
+
+def find_average_for_clustered_lines(lines,number_of_lines,clusters, number_of_clusters, mean_clustered_lines):
+    """Finds average for every cluster and calculates lines that is mean for each cluster
+
+    Args:
+        lines (numpy array, dtype=np.int32): Array of lines, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+        number_of_lines (integer):  Number of lines in array
+        clusters (numpy array, dtype=np.int32):  Array of clusters marks for each lines, outliers have cluster mark -1
+        number_of_clusters (integer): Number of clusters
+        mean_clustered_lines (numpy array, dtype=np.single): Array of lines where each line is mean for one cluster, one line is represented with two adjacent elements in array where first one is theta in degrees and second one is rho
+    """
+    function=board_finder_lib.findAverageForClusteredLines
+    function.argtypes=[np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32,np.ctypeslib.ndpointer(ctypes.c_int32), ctypes.c_int32,np.ctypeslib.ndpointer(ctypes.c_float)]
+    function(lines,number_of_lines,clusters,number_of_clusters,mean_clustered_lines)
+
+def calculate_Hough_space_matrix(picture,width,height,rho,theta,hough_space_matrix):
+    """Fills Hough space matrix for image that is preprocessed with edge detector and return maximum value from matrix 
+
+    Args:
+        picture (numpy array, dtype=np.uint8):  Array that contains pixel values of image, pixel values are 0 and 255
+        width (integer):  Width of image in pixels
+        height (integer): Height of image in pixels
+        rho (integer): Maximum value of rho
+        theta (integer): Maximum value of theta
+        hough_space_matrix (numpy array, dtype=np.int32): Array that represents Hough space matrix of size [2*rho,theta]
+
+    Returns:
+        (integer): Maximum number of voters from Hough space matrix
+    """
+    function=board_finder_lib.calculateHoughSpaceMatrix
+    function.argtypes=[np.ctypeslib.ndpointer(ctypes.c_uint8),ctypes.c_int32,ctypes.c_int32,ctypes.c_int32,ctypes.c_int32,np.ctypeslib.ndpointer(ctypes.c_int32)]
+    function.restype = ctypes.c_int32
+    return function(picture,width,height,rho,theta,hough_space_matrix)
+
+def find_lines_above_voters_threshold(hough_space_matrix,rho_max,lines,number_of_voters, max_number_of_lines, theta_max=MAX_ANGLE):
+    function=board_finder_lib.findLinesAboveVotersThreshold
+    function.argtypes=[np.ctypeslib.ndpointer(ctypes.c_int32),ctypes.c_int32,np.ctypeslib.ndpointer(ctypes.c_int32),ctypes.c_int32,ctypes.c_int32,ctypes.c_int32]
+    function.restype = ctypes.c_int32
+    return function(hough_space_matrix,rho_max,lines,number_of_voters, max_number_of_lines, theta_max)
+
+def hough_line_detector(picture,width, height, lines,votersThreshold=0.5,max_number_of_lines=MAX_LINES):
+    function=board_finder_lib.houghLineDetector
+    function.argtypes=[np.ctypeslib.ndpointer(ctypes.c_uint8),ctypes.c_int32,ctypes.c_int32,np.ctypeslib.ndpointer(ctypes.c_int32),ctypes.c_float,ctypes.c_int32]
+    function.restype = ctypes.c_int32
+    return function(picture,width, height, lines,votersThreshold,max_number_of_lines)
+
+
+
--- R&D/testDLL.py
+++ R&D/testDLL.py
@@ -1,26 +1,60 @@
-import ctypes
+import sys
+sys.path.append(".")
+
+from CppDllWrap import boardFinder
 import cv2
 import numpy as np
 import configparser
+import math
+
+ROLLING_RANGE=10
+PEAK_WIDTH=30
+
+def draw_lines_on_image_and_display(lines,number_of_lines,image,color_RGB=255, display=True):
+    for index in range(0,number_of_lines):
+        rho=lines[2*index]
+        theta=(lines[2*index+1]*math.pi/180)
+        a=math.cos(theta)
+        b=math.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 - 1500 * b)
+        y1 = int(y0 + 1500 * a)
+        x2 = int(x0 + 1500 * b)
+        y2 = int(y0 - 1500 * a)
+        cv2.line(image,(x1,y1),(x2,y2),color_RGB,2)
+
+    if display:
+        cv2.imshow('Image',image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
-def mask_image_C(image,image_function,image_data_type=ctypes.c_uint8):
-    image_function.argtypes=[np.ctypeslib.ndpointer(image_data_type),ctypes.c_int32,ctypes.c_int32]
-
-    return image_function(image,image.shape[0],image.shape[1])
-
-
-
-
-if __name__ =="__main__":    
+if __name__ =="__main__":
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read('config.ini')    
+    image = cv2.imread(config['Resources']['image_lines'],cv2.IMREAD_GRAYSCALE)
+    image=cv2.Canny(image,140,200)
+    lines=np.zeros(boardFinder.MAX_LINES,dtype=np.int)
+    voters_threshold=0.5
+    number_of_lines=boardFinder.hough_line_detector(image,image.shape[1],image.shape[0],lines,voters_threshold,boardFinder.MAX_LINES)
 
-    testLib=ctypes.CDLL(config['Resources']['dll'])
-    image = cv2.imread(config['Resources']['image'])
-    #print(image.size)
+    horizontal_lines=np.zeros(2*number_of_lines,dtype=np.int)
+    vertical_lines=np.zeros(2*number_of_lines,dtype=np.int)
 
-    mask_image_C(image,testLib.doPicture)
+    number_of_horizontal_lines,number_of_vertical_lines=boardFinder.find_two_biggest_clusters_of_lines(lines,number_of_lines,ROLLING_RANGE,PEAK_WIDTH,boardFinder.MAX_LINES,horizontal_lines,vertical_lines)
 
-    cv2.imshow('image',image)
-    cv2.waitKey(0)
+    horizontal_clusters=np.zeros(2*number_of_horizontal_lines,dtype=np.int)
+    vertical_clusters=np.zeros(2*number_of_vertical_lines,dtype=np.int)
+
+    number_of_horizontal_clusters=boardFinder.lines_DBSCAN(horizontal_lines,number_of_horizontal_lines,horizontal_clusters)
+    number_of_vertical_clusters=boardFinder.lines_DBSCAN(vertical_lines,number_of_vertical_lines,vertical_clusters)
+
+    mean_horizontal_lines=np.zeros(2*number_of_horizontal_clusters,dtype=np.single)
+    mean_vertical_lines=np.zeros(2*number_of_vertical_clusters,dtype=np.single)
+
+    boardFinder.find_average_for_clustered_lines(horizontal_lines,number_of_horizontal_lines,horizontal_clusters,number_of_horizontal_clusters,mean_horizontal_lines)
+    boardFinder.find_average_for_clustered_lines(vertical_lines,number_of_vertical_lines,vertical_clusters,number_of_vertical_clusters,mean_vertical_lines)
+
+    draw_lines_on_image_and_display(mean_horizontal_lines,number_of_horizontal_clusters,image,150,False)
+    draw_lines_on_image_and_display(mean_vertical_lines,number_of_vertical_clusters,image,150)
