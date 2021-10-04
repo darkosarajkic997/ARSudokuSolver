59ab82d178550f4fb32b8cc6da4165ae10c8af2e
Darko <darkosarajkic997@gmail.com>
2021-10-04 16:02:28 +0200

    Find intersection for two lines function added

--- DLLs/BoardFinderLibrary/RANSACHomography/RANSACHomography.cpp
+++ DLLs/BoardFinderLibrary/RANSACHomography/RANSACHomography.cpp
@@ -1,1 +1,34 @@
+#include "pch.h"
 #include "RANSACHomography.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+bool checkIfDetIsZero(float det, float epsilon = 0.00005)
+{
+	if (det < epsilon)
+		return true;
+	return false;
+}
+
+bool findIntersectionOfTwoIntersectingLines(float rho1, float theta1, float rho2, float theta2, float* x, float* y)
+{
+	float sinTheta1, sinTheta2, cosTheta1, cosTheta2, detA;
+	sinTheta1 = sin(theta1);
+	sinTheta2 = sin(theta2);
+	cosTheta1 = cos(theta1);
+	cosTheta2 = cos(theta2);
+	detA = cosTheta1 * sinTheta2 - sinTheta1 * cosTheta1;
+	if (checkIfDetIsZero(detA))
+	{
+		return false;
+	}
+	*x = (rho1 * sinTheta2 - rho2 * sinTheta1) / detA;
+	*y = (-rho1 * cosTheta2 + rho2 * cosTheta1) / detA;
+
+	return true;
+}
+
+void findHomograpyFromTwoLinesClusters(float* horizontalLines, int numberOfHorizontalLines, float* verticalLines, int numberOfVerticalLines, float* homographyMatrix)
+{
+	
+}
